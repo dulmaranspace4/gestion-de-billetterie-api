b59bb0abf2a21e59706a457928a5d387669cbbf0
dulmaranspace4 <serulmrls@hotmail.com>
2025-09-06 11:55:10 -0700

    Improve logging middleware formatting

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware for logging requests
 app.use((req, res, next) => {
-    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+    console.log(`[${new Date().toISOString()}] - ${req.method} ${req.url}`);
     next();
 });
 
