076f59f3fa3d9d8cd5a4700531b36241efc6f508
dulmaranspace4 <serulmrls@hotmail.com>
2025-09-06 11:52:06 -0700

    Update MongoDB connection string for better security practices by using environment variables.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/billetterie', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/billetterie', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Connection error to MongoDB:', err));
 
