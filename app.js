c41c3fc0eb20376126ca8dcd19d06fbf8b06fbaa
dulmaranspace4 <serulmrls@hotmail.com>
2025-09-06 11:52:28 -0700

    Add a simple logging middleware to track requests

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const eventRoutes = require('./routes/eventRoutes');
 const ticketRoutes = require('./routes/ticketRoutes');
 
 const app = express();
+
+// Middleware for logging requests
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 app.use(express.json());
 app.use('/api/events', eventRoutes);
 app.use('/api/tickets', ticketRoutes);
